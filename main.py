3c693458920b047e69a29147ffbafeb894da2d64
Tex <xavier.bolivarhazboun@uoit.net>
2021-09-18 00:34:32 -0400

    - Login to self-serve portal working - Defined RegularExpression string to find the semesters in the year. - Commented out initial auth. Not sure I really need it. I'll leave for now and delete later if need be.

--- main.py
+++ main.py
@@ -5,15 +5,28 @@ import re
 
 uuid_regex = 'sessionDataKey=(\w{8}.\w{4}.\w{4}.\w{4}.\w{12})'
 
+browser_session = requests.Session()
+# preauth = 'http://portal.mycampus.ca/cp/home/login'
+# response = requests.post(preauth, data={'username': secret.student_number,
+#                                         'password': secret.password,
+#                                         'uuid': secret.uuid
+#                                         }
+#                          )
 
 # Let's start with getting our session I.D.
 next_url = 'https://ssp.mycampus.ca/StudentRegistrationSsb/ssb/registrationHistory/registrationHistory?mepCode=UOIT'
-response = requests.get(next_url)
-response = response.text
-session_key = re.search(uuid_regex,response)
-print(session_key.group(1))
-#Now we must get session key.
+response = browser_session.get(next_url)
+response_text = response.text
 
+session_key = re.search(uuid_regex, response_text).group(1)
 
+# Now we can login to the self-serve portal
 
+login_post_url = 'https://eidp.mycampus.ca/commonauth'
+response = browser_session.post(login_post_url, data={'username': secret.student_number,
+                                               'password': secret.password,
+                                               'sessionDataKey': session_key
+                                                      }
+                                )
 
+semesters_list_regex = '<option value="(\d+)" >([A-Za-z]+(?:(?:\/[A-Za-z]+ \d{4})|(?: \d{4})))'
